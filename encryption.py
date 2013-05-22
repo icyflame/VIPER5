addb8a00cb2f548b6751acd4c70e9d7806f4d0e7
IcyFlame <kannan.siddharth12@gmail.com>
2013-05-22 15:03:57 +0530

    added the code for AES encryption to the encryption.py file. BUt it says that the plain text must be a multiple of 16. this will not work. will have to look for some other encryption

--- encryption.py
+++ encryption.py
@@ -7,11 +7,58 @@
 ##
 ##created on 20th May, 2013
 
+from Crypto.Cipher import *
+
+global encryptor
+global decryptor
+
+key1 = 'siddharth kannan'
+
+encryptor = AES.new(key1)
+decryptor = AES.new(key1)
 
 def decryptAll():
 
-    pass
+    filin = open('viper','r')
+    filout = open('temp','w')
+
+    for i in filin:
+
+        while i[-1] == '\n' or i[-1] == '\r':
+
+            i = i[:-1]
+
+        filout.write(decryptor.decrypt(i) + '\n')
+
+
+    filin.close()
+
+    filout.close()
+
+    import os
+    os.remove('viper')
+    os.rename('temp','viper')
+
+    return True
 
 def encryptAll():
+    
+    filin = open('viper','r')
+    filout = open('temp','w')
 
-    pass
+    for i in filin:
+
+        while i[-1] == '\n' or i[-1] == '\r':
+
+            i = i[:-1]
+
+        filout.write(encryptor.encrypt(i) + '\n')
+
+
+    filin.close()
+
+    filout.close()
+
+    import os
+    os.remove('viper')
+    os.rename('temp','viper')
