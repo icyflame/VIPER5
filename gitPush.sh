de3f25d389abe79dbd40dd87b94619a32e41f95b
IcyFlame <kannan.siddharth12@gmail.com>
2013-05-27 22:19:14 +0530

    modified gitPush.sh

--- gitPush.sh
+++ gitPush.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-echo 'current status:'
-echo '=================================='
-echo '=================================='
-
 git status
 
-echo 'adding all the files in the folder'
+echo
+echo
 
-git add .
 
-echo '=================================='
-echo '=================================='
-
-echo "Enter the commit message:"
-read commitMes
-
-git commit -m "$commitMes"
-
-echo '=================================='
-echo '=================================='
-echo 'the changes have been comitted:'
-echo '=================================='
-echo '=================================='
 git log
 
 
